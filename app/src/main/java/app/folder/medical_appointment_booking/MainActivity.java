c4988b539a58633354942cb60d23e6d320616e39
Hoang Thuy Vo Anh <hoangtvasr150780@gmail.com>
2022-03-14 12:26:05 +0700

    patient

---
+++ app/src/main/java/app/folder/medical_appointment_booking/MainActivity.java
@@ -0,0 +1,41 @@
+package app.folder.medical_appointment_booking;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.JsonRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView textView;
+    private RequestQueue queue;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        textView = findViewById(R.id.txtError);
+
+        queue = Volley.newRequestQueue(this);
+    }
+
+    public void MoveToHistoryAppointmentPage(View view) {
+        Intent intent = new Intent(this, PatientAppointmentActivity.class);
+        startActivity(intent);
+    }
+}
